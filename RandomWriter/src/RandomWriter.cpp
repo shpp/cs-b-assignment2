e07f3e6628fc816be61690a860844b07357bf9ee
antony <amativos@gmail.com>
2015-08-13 18:21:00 +0300

    Initial commit

---
+++ RandomWriter/src/RandomWriter.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "console.h"
+#include "map.h"
+#include "random.h"
+#include "strlib.h"
+#include "vector.h"
+using namespace std;
+
+int main() {
+    // TODO: fill in the code
+	return 0;
+}
---
+++ WordLadder/src/WordLadder.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "console.h"
+#include "lexicon.h"
+#include "queue.h"
+#include "simpio.h"
+#include "vector.h"
+using namespace std;
+
+int main() {
+    // TODO: fill in the code
+	return 0;
+}
