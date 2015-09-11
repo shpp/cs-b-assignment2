566c9a29cff9ed40ee745c3b02176280ecbe164d
ekuryatenko <kuryatenko@ukr.net>
2015-09-11 11:15:18 +0300

    First commit

--- RandomWriter/src/RandomWriter.cpp
+++ RandomWriter/src/RandomWriter.cpp
@@ -1,4 +1,4 @@
-#include <fstream>
+﻿#include <fstream>
 #include <iostream>
 #include <string>
 #include "console.h"
@@ -6,9 +6,292 @@
 #include "random.h"
 #include "strlib.h"
 #include "vector.h"
+#include "simpio.h"
+#include "filelib.h"
+#include "stack.h"
+#include "queue.h"
+#include "timer.h"
+
 using namespace std;
 
+/* FILE READING */
+string fileInput(string promptText);
+int levelInput(string promptText);
+
+Map<string, Vector<string>> getFileMap(istream & infile, int modelLevel);
+string getLevelSeed(istream & infile,
+                    char seedChar,
+                    int modelLevel,
+                    Stack<char>& stack);
+string getSymbolAfterSeed(istream & infile);
+void returnCharsToStream(istream & infile,Stack<char>& stack);
+
+void modifyMap(Map<string, Vector<string>>& mainMap,
+               string seed,
+               string nextSymbol);
+/* Level 0-model Text GENERATION */
+void generate_0_levelText(Map<string, Vector<string>>& fileMap, int printSymbQty);
+Map<string, Vector<string>> getStatisticMap(Map<string, Vector<string>>& fileMap);
+
+/* Level N-model Text GENERATION */
+void generate_N_levelText(Map<string, Vector<string>>& fileMap, int printSymbQty);
+string getFrequentSeed(Map<string, Vector<string>>& fileMap);
+string getRandomSymbol(string key, Map<string, Vector<string>>& map);
+
+// Console settings
+void consoleSettings();
+const string programTitle = "RandomWriter";
+const bool consoleEcho = false;
+
 int main() {
-    // TODO: fill in the code
-	return 0;
+    consoleSettings();
+
+    /* User enters name of file from project directory */
+    string fileName = fileInput("Enter file name: ");
+    //string fileName = "MiddleMarch.txt";
+    //string fileName = "Readme.txt";
+
+    int modelLevel = levelInput("Enter model level range (1-10): ");
+    /* Generated text symbols qty */
+    int resultTextSymbolsQty = 2000;
+
+    cout << "Processing..." << endl;
+    Timer timer1(true);
+    cout << "=========================================" << endl;
+    /* Prepare input stream object   */
+    ifstream infile;
+    /* Lunch stream of file reading   */
+    infile.open(fileName.c_str());
+    /* Main read text process - seeds map creating */
+    Map<string, Vector<string>> fileMap = getFileMap(infile, modelLevel);
+    /* If whole file is read program closes stream */
+    infile.close();
+
+    cout << "MAP BUILD TIME IS " << timer1.elapsed() << endl;
+
+    timer1.stop();
+    Timer timer2(true);
+
+    cout << "=========================================" << endl;
+
+    if(modelLevel > 0){
+        generate_N_levelText(fileMap, resultTextSymbolsQty);
+    }else{
+        generate_0_levelText(fileMap, resultTextSymbolsQty);
+    }
+
+    cout << endl;
+    cout << "=========== END OF GENERATION ==========="<< endl;
+    cout << "TEXT BUILD TIME IS " << timer2.elapsed() << endl;
+    timer2.stop();
+
+    return 0;
+}
+
+/* Main read text process - seeds map creating */
+Map<string, Vector<string>> getFileMap(istream & infile, int modelLevel){
+        Map<string, Vector<string>> resultMap;
+        char startSeedChar;
+        while ((startSeedChar = infile.get()) != EOF) {
+            /* Stack for chars from seed */
+            Stack<char> stack;
+            /* Take chars from stream to build seed for required level */
+            string seed = getLevelSeed(infile, startSeedChar, modelLevel, stack);
+            /* Get next symbol after such seed */
+            string nextSymbol = getSymbolAfterSeed(infile);
+            /* Return chars from stack to stream */
+            returnCharsToStream(infile, stack);
+            /* Fills resultMap by obtained seed as key and nextSymbol as result  */
+            modifyMap(resultMap, seed, nextSymbol);
+        }
+        return resultMap;
+}
+
+/* This function calls after seed getting from stream
+ * Returns next symbol, after seed chars, from stream */
+string getSymbolAfterSeed(istream & infile){
+    string result = "";
+    char nextChar = infile.peek();
+    if(nextChar != EOF){
+        result = charToString(nextChar);
+    }
+    return result;
+}
+
+/* Get seedChar - next char from stream, and builds seed of
+ * required length */
+string getLevelSeed(istream & infile,
+                    char seedChar,
+                    int modelLevel,
+                    Stack<char>& stack){
+    /* seed begins from seedChar */
+    string result = charToString(seedChar);
+    if(modelLevel > 0){
+        for(int i = 1; i < modelLevel; i++){
+            char nextChar;
+            if ((nextChar = infile.get()) != EOF){
+                /* Adds next text symbols to seed */
+                result += charToString(nextChar);
+                /* Stores taken seed chars to stack
+                 * It'll enables to return them back to stream letter */
+                stack.push(nextChar);
+            }
+        }
+    }
+    return result;
+}
+
+/* Modifies main file map - adds seeds and modifies nexy chars vectors */
+void modifyMap(Map<string, Vector<string>>& mainMap,
+               string seed,
+               string nextSymbol){   
+    if(mainMap.containsKey(seed)){
+        /* Modifies map entry - adds next symbol to vector */
+        mainMap[seed].add(nextSymbol);
+    }else{
+        /* Adds new map entry with such next symbol value */
+        Vector<string> newSeedVector;
+        newSeedVector.add(nextSymbol);
+        mainMap.put(seed, newSeedVector);
+    }
+}
+
+/* This function calls after seed creation by getLevelSeed
+ * Return chars ffrom stack to stream */
+void returnCharsToStream(istream & infile,Stack<char>& stack){
+    while(!stack.isEmpty()){
+        char symbol = stack.pop();
+        infile.putback(symbol);
+    }
+}
+
+/* Unfortunately I haven't managed to make it more rapid - dedaline time is coming
+ *
+ * 0-level generating depends of
+ * - every new symbol of text appear after seed due to it's statistic probability */
+void generate_0_levelText(Map<string, Vector<string>>& fileMap, int printSymbQty){
+    string startSeed = getFrequentSeed(fileMap);
+    cout << "THE MOST FREQUENT SEED IS: \"" << startSeed << "\"" << endl;
+    cout << "============= GENERATED TEXT ============" << endl;
+    cout <<startSeed;
+
+    int counter = 0;
+    /* For Level-0 Model every next symbol apear in text due to it's probability */
+    /* Statistic map saves all next symbols as a value vector
+     * Randomly taking from this vector gives probability for this symbol */
+    Map<string, Vector<string>> statisticMap = getStatisticMap(fileMap);
+
+    while(counter < printSymbQty){
+        string nextSymbol = getRandomSymbol("someDefalutKey", statisticMap);
+        if(nextSymbol == "no symbols"){
+            cout << endl;
+            cout << "PROGRAM STOPS DUE TO BREAK CONDITION";
+            break;
+        }
+        cout << nextSymbol;
+        counter++;
+    }
+    cout <<  endl;
+}
+
+/* Prepares map with single key and single next symbols vector
+ * Vector stores all possible next symbols from text
+ * Random taking from this vector returns symbols due to their probabylity */
+Map<string, Vector<string>> getStatisticMap(Map<string, Vector<string>>& fileMap){
+    /* This key is not important for our tasks */
+    string key = "someDefalutKey";
+    /* Vector of all next symbols */
+    Vector<string> totalVec;
+    for(string key: fileMap){
+        Vector<string> keyVec = fileMap[key];
+        for(string i: keyVec){
+            totalVec.add(i);
+        }
+    }
+    Map<string, Vector<string>> result;
+    result.put(key, totalVec);
+    return result;
+}
+
+/* N-level generating depends of
+ * - every new symbol of text appear after such seed more frequently */
+void generate_N_levelText(Map<string, Vector<string>>& fileMap, int printSymbQty){
+    string startSeed = getFrequentSeed(fileMap);
+    cout << "THE MOST FREQUENT SEED IS: \"" << startSeed << "\"" << endl;
+    cout << "============= GENERATED TEXT ============" << endl;
+    cout <<startSeed;
+
+    int counter = 0;
+    while(counter < printSymbQty){
+        string nextSymbol = getRandomSymbol(startSeed, fileMap);
+        if(nextSymbol == "no symbols"){
+            cout << endl;
+            cout << "NO NEXT SYMBOL CONDITION";
+            break;
+        }
+        cout <<nextSymbol;
+        startSeed.erase(0,1);
+        startSeed += nextSymbol;
+        counter++;
+    }
+}
+
+/* The most frequent seed is seed with bigest vector */
+string getFrequentSeed(Map<string, Vector<string>>& fileMap){
+    int maxSize = 0;
+    string result = "";
+    for(string key: fileMap){
+        int keySize = fileMap[key].size();
+        if(keySize > maxSize){
+            result = key;
+            maxSize = keySize;
+        }
+    }
+    return result;
+}
+
+/* Next symbol is get from seed statistic vector due to its probability in vector */
+string getRandomSymbol(string key, Map<string, Vector<string>>& map){
+    if(map.containsKey(key)){
+        int thisKeyVecSize = map[key].size();
+        int randomInt = randomInteger(0, (thisKeyVecSize - 1));
+        Vector<string> vec = map[key];
+        return vec[randomInt];
+    }else{
+        return "no symbols";
+    }
+}
+
+
+/* User input function
+ * Defines if this file exist in project directory */
+string fileInput(string promptText){
+    while(true){
+        string result = getLine(promptText);
+        if(fileExists(result)){
+            return result;
+            break;
+        }else{
+            cout << "Unfortunately your input is failed" << endl;
+        }
+    }
+}
+
+/* User input function
+ * Defines if this model level value is correct */
+int levelInput(string promptText){
+    while(true){
+        int result = getInteger(promptText);
+        if((result >= 0) && ((result <= 10))){
+            return result;
+            break;
+        }else{
+            cout << "Unfortunately your input is failed" << endl;
+        }
+    }
+}
+
+void consoleSettings(){
+    setConsoleWindowTitle(programTitle);
+    setConsoleEcho(consoleEcho);
 }
--- WordLadder/src/WordLadder.cpp
+++ WordLadder/src/WordLadder.cpp
@@ -4,9 +4,258 @@
 #include "queue.h"
 #include "simpio.h"
 #include "vector.h"
+#include "strlib.h"
+
 using namespace std;
 
+/*Functions prototypes  */
+void wordLadderGame();
+void userInput(string& startWord, string& endWord, Lexicon englishWords);
+string validInput(string prompt, Lexicon englishWords);
+void addLadderToQueue(Vector<string> vec,
+                      Queue<Vector<string>> & mainQueue,
+                      Lexicon & wordsInQueue);
+Vector<string> getNewValidWords(string inputWord, Lexicon & englishWords);
+string nextFromAlphabet(string inputWord, int index);
+Vector<string> mainQueueProcess(Queue<Vector<string>> & mainQueue,
+                      Lexicon & wordsInQueue,
+                      Lexicon & englishWords,
+                      string endWord);
+void programOutput(Vector<string>& resultLadder);
+
+/* Program console settings */
+void consoleSettings();
+const string programTitle = "WordLadder";
+const bool consoleEcho = false;
+
 int main() {
-    // TODO: fill in the code
+    consoleSettings();
+    /* "WordLadder game" function
+     * The main task is to modify words from user due to game rules
+     * At the end of process result cosequence of word have be printed
+     */
+    wordLadderGame();
+
 	return 0;
+}
+
+/* "WordLadder game" function
+ * The main task is to get pair of words from user - start word and end word
+ * Function search variant to modify start word into end word
+ * The main rules of game modifications is:
+ * - each new mofification of start word can change only one letter in it
+ * - each word is valid english word
+ * Words from user must have same length
+ * At the end of process result cosequence of word have be printed
+ */
+void wordLadderGame(){
+    Lexicon englishWords("EnglishWords.txt");
+
+    /* User input */
+    string startWord = "";
+    string endWord = "";
+    userInput(startWord, endWord, englishWords);
+
+    /* Main program store - stores all posible dicisions of the main task */
+    Queue<Vector<string>> mainQueue;
+    /* Creates lexicon of all words which queue contain */
+    Lexicon wordsInQueue;
+    /* Creates start possible variant of the task */
+    Vector<string> newLadder(1, startWord);
+    /* Fills main program queue by start possible variant of the task */
+    addLadderToQueue(newLadder, mainQueue, wordsInQueue);
+    /* Main program process */
+    Vector<string> resultLadder = mainQueueProcess(mainQueue,
+                                                   wordsInQueue,
+                                                   englishWords,
+                                                   endWord);
+    programOutput(resultLadder);
+}
+
+/* Main program process
+ * - get next ladder
+ * - check there last word for end case
+ * - get mofified variants of this word
+ * - adds them into vectors
+ * - adds these vectors to the queue
+ * @param mainQueue - reference of main queue of program - store of possible
+ *                    program decision variants
+ * @param wordsInQueue - reference - store of  all posible words from queue
+ * @param wordsInQueue - reference - store of possible valid english words
+ * @param endWord - copy of the result user word
+ *
+ * @return - vector of first program decision, ladder of words from start word to end word
+ */
+Vector<string> mainQueueProcess(Queue<Vector<string>> & mainQueue,
+                      Lexicon & wordsInQueue,
+                      Lexicon & englishWords,
+                      string endWord){
+
+    Vector<string> resultLadder;
+    while (!mainQueue.isEmpty()){
+        /* Gets next queue element */
+        Vector<string> queueLastLadder = mainQueue.dequeue();
+        /* Gets word from this ladder, which have being put last there */
+        string lastLadderWord = queueLastLadder[(queueLastLadder.size() - 1)];
+        /* Checks an end case of programm */
+        if (lastLadderWord == endWord){
+            /* put this ladder into the programm output */
+            resultLadder = queueLastLadder;
+            break;
+        }else{
+            /* Gets vector of all new possible variants of this word */
+            Vector<string> newWordsVec = getNewValidWords(lastLadderWord, englishWords);
+            /* Adds all new values to new ladders, and adds them to the queue */
+            for(string modifiedWord: newWordsVec){
+                /* Each new ladder is made as a copy of the taken ladder */
+                Vector<string> nextNewLadder = queueLastLadder;
+                /* Put new mofified word into new copy */
+                nextNewLadder.add(modifiedWord);
+                /* Adds such new ladder into the queue */
+                addLadderToQueue(nextNewLadder, mainQueue, wordsInQueue);
+            }
+        }
+    }
+    return resultLadder;
+}
+
+/* Adds each new ladder to main queue
+ * Creates lexicon of the queue's words
+ *
+ * Modifies mainQueue and queueWordList by reference
+ *
+ * @param vec - ladder to put into queue
+ * @param mainQueue - main queue of the programm
+ * @param queueWordsList - queue lexicon, saves all kinds of words from queue  */
+void addLadderToQueue(Vector<string> vec,
+                      Queue<Vector<string>> & mainQueue,
+                      Lexicon & queueLexicon){
+    /* For this version suppose that such vector has only one new kind of word - last word */
+    string lastWord = vec[(vec.size() - 1)];
+    /* If queue lexicon doesn't contain such word... */
+    if(!queueLexicon.contains(lastWord)){
+        /* ...then this word is put to queue lexicon */
+        queueLexicon.add(lastWord);
+        /* And add this vec to queue */
+        mainQueue.enqueue(vec);
+    }
+}
+
+/* Creates list of all possible input word modifications
+ * Get word and modifies one letter from it
+ * If modified word is valid - save it into result vector
+ * inputWord isn't saved into result
+ *
+ * @param inputWord - simple word
+ * @param englishLexicon - vocabulary of valid english words
+ *
+ * @return - vector of all possible modifications */
+Vector<string> getNewValidWords(string inputWord, Lexicon & englishLexicon){
+    Vector<string> resultVector;
+    /* Counter of modification proccess */
+    int modificationIndex = 0;
+    string modificatedWord = "defalut";
+    /* loop of new words generating
+     * It stops if last modification was equal empty string */
+    while (modificatedWord != ""){
+        /* Gets new modification of input word */
+        modificatedWord = nextFromAlphabet(inputWord, modificationIndex);
+        /* Checks new modification
+         * If it's starts from letter - for case of damaged words
+         * like "-ba" during input */
+        if(isalpha(modificatedWord[0])){
+            /* If new modification dosen't equal input word */
+            if(modificatedWord != inputWord){
+                /* If new modification is valid english word */
+                if(englishLexicon.contains(modificatedWord)){
+                   resultVector.add(modificatedWord);
+                }
+            }
+        }else{
+            break;
+        }
+        /* Shifts counter to direct modification process */
+        modificationIndex++;
+    }
+    return resultVector;
+}
+
+/* Modifies each letter in input word due to alphabet
+ *
+ * @param inputWord - simple word
+ * @param index - gives position of modification. If value of index bigger
+ * then possible variants of modifications - function returns ""
+ *
+ * @return - modification of input word or "" if there are no modifications for
+ * such index */
+string nextFromAlphabet(string inputWord, int index){
+    string alphabet = "abcdefghijklmnopqrstuvwqxyz";
+    /* Initiate result string */
+    string outputWord = inputWord;
+    /* Index of the next letter posirion to be changed */
+    int letterIndex = index / alphabet.length();
+    /* Index of the next letter from alphabet to be put into modification position */
+    int alphabetIndex = index % alphabet.length();
+    /* While process index alows to generate modifications */
+    if(index < ((alphabet.length()) * (inputWord.length()))){
+        /* Create new modification - change letter at this position */
+        outputWord[letterIndex] = alphabet[alphabetIndex];
+        return outputWord;
+    }else{
+        /* If process index value is more then possible return end case value  */
+        return "";
+    }
+}
+
+void userInput(string& startWord, string& endWord, Lexicon englishWords){
+    //startWord = "sleep";
+    //endWord = "awake";
+
+    //startWord = "airplane";
+    //endWord = "tricycle";
+
+    //startWord = "work";
+    //endWord = "play";
+
+    //startWord = "code";
+    //endWord = "data";
+
+    //startWord = "never";
+    //endWord = "event";
+
+    startWord = validInput("Enter start word: ", englishWords);
+    endWord = validInput("Enter end word: ", englishWords);
+}
+
+string validInput(string prompt, Lexicon englishWords){
+    while(true){
+            string result = getLine(prompt);
+            if(englishWords.contains(result)){
+                return result;
+                break;
+            }else{
+                cout << "Unfortunately your input is failed" << endl;
+            }
+     }
+}
+
+/* Programm output - prints results of process */
+void programOutput(Vector<string>& resultLadder){
+    /* End case - if there no variants of modification */
+    if(resultLadder.isEmpty()){
+        cout << "No ladder found";
+    }else{
+        /* Loop of the result printing */
+        for(int i = 0; i < (resultLadder.size() - 1); i++){
+            cout << resultLadder[i];
+            cout << " -> ";
+        }
+        cout << resultLadder[(resultLadder.size() - 1)];
+    }
+}
+
+/* Make console output window more convenient */
+void consoleSettings(){
+    setConsoleWindowTitle(programTitle);
+    setConsoleEcho(consoleEcho);
 }
